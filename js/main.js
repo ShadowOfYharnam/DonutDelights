704f6ad861fd572ff23672ababceb992504a6f0b
ShadowOfYharnam <shadow.hunter_96@yahoo.com>
2016-05-16 17:23:22 -0500

    made the google able to track page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
 $(document).ready(function(){
+    //didnt really have any java script, except for the validation and that was one funcion that went inside the form element
+
+
     
-   $("#contactInfo").validate();
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-77239059-2', 'auto');
+  ga('send', 'pageview');
+
 
 });
